246f75497024350acff8ea2ea641804c3ae69ea1
Miszak <miszak18@gmail.com>
2019-03-27 10:53:59 +0100

    fixed bug with parsing date

--- src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO USER (ID_USER, EMAIL, PASSWORD, CITY, FIRST_NAME, LAST_NAME ) VALUES ('1', 'miszak18@gmail.com', 'admin', 'Warszawa', 'Joe', 'Doe');
-
-
-INSERT INTO EVENT (ID, EVENT_DATE, EVENT_DESCRIPTION, EVENT_NAME, EVENT_CITY ) VALUES ('1', '2019-08-02', 'lorem ipsum', 'Dupa festiwal', 'Szczebrzeszyn');
-INSERT INTO EVENT (ID, EVENT_DATE, EVENT_DESCRIPTION, EVENT_NAME, EVENT_CITY ) VALUES ('2', '2019-11-02', 'lorem ipsum', 'jakis festiwal', 'Szczecin');
-INSERT INTO EVENT (ID, EVENT_DATE, EVENT_DESCRIPTION, EVENT_NAME, EVENT_CITY ) VALUES ('3', '2019-05-02', 'lorem ipsum', 'majówka festiwal', 'Poznań');
-INSERT INTO EVENT (ID, EVENT_DATE, EVENT_DESCRIPTION, EVENT_NAME, EVENT_CITY ) VALUES ('4', '2020-01-02', 'lorem ipsum', 'festiwal piwa', 'Warszawa');
-INSERT INTO EVENT (ID, EVENT_DATE, EVENT_DESCRIPTION, EVENT_NAME, EVENT_CITY ) VALUES ('5', '2019-07-21', 'lorem ipsum', 'Festiwal piergoów', 'Kraków');
